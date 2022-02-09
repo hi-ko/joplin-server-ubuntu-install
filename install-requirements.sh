628db2aa54b2a8ee0e2cdad9c7a10b407c6a80ad
Heiko Robert <heiko.robert@ecm4u.de>
2022-02-09 16:18:00 +0100

    first commit for new yarn based build

---
+++ install-requirements.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+USER_ID=`id -u`
+
+if [[ "$USER_ID" != "0" ]]
+then
+  echo "$0: You must run this script as root: $USER_ID"
+  exit 1
+fi
+
+if [[ -f /etc/default/joplinrc ]];then
+    . /etc/default/joplinrc
+else
+    echo ""
+fi
+
+JOPLIN_HOME="$JOPLIN_HOME:-/opt/joplin}"
+
+apt update
+apt -y install vim git
+
+## install nodejs 16
+# from https://github.com/nodesource/distributions
+# remove old version 
+apt-get -y purge nodejs npm
+# add repo
+apt-get -y install curl dirmngr apt-transport-https lsb-release ca-certificates
+curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash -
+
+apt-get install -y nodejs
+# in case we need to compile modules
+apt -y install build-essential python
+
+# install the Yarn package manager
+#curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | gpg --dearmor | sudo tee /usr/share/keyrings/yarnkey.gpg >/dev/null
+#echo "deb [signed-by=/usr/share/keyrings/yarnkey.gpg] https://dl.yarnpkg.com/debian stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+# apt-get update && apt-get install yarn
+
+# yarn is self contained in nodejs 16 but needs to be enabled:
+corepack enable
+
+JOPLIN_USER=joplin
+id -u $JOPLIN_USER &>/dev/null || sudo useradd --create-home --shell /bin/bash $JOPLIN_USER
+
+mkdir -p $JOPLIN_HOME
+chmod $JOPLIN_USER: $JOPLIN_HOME
---
+++ joplin.sh
@@ -0,0 +1,120 @@
+#!/bin/bash
+# adapted from  https://github.com/laurent22/joplin/blob/dev/Dockerfile.server
+
+if [[ -f /etc/default/joplin ]];then
+    . /etc/default/joplin
+fi
+
+GITDIR="${GITDIR:-$HOME/joplin}"
+WORKDIR="${WORKDIR:-$HOME/build}"
+JOPLIN_HOME="${JOPLIN_HOME:-/opt/joplin}"
+SERVICE_NAME=joplin-server
+
+COPY(){
+    cd $WORKDIR
+    SRC=$GITDIR/$1
+    DEST=$2
+
+    if [ ! -d $DEST ]; then
+        mkdir -p $2
+    fi
+    if [ -d "$SRC" ]; then
+        SRC="$SRC/"
+    fi
+    rsync -a $SRC $2/
+}
+
+checkout_latest(){
+    GITDIR_PARENT=$(realpath $GITDIR/..)
+    mkdir -p $GITDIR_PARENT && cd $GITDIR_PARENT
+    if [ ! -d $GITDIR ];then
+        git clone https://github.com/laurent22/joplin.git
+    fi
+    cd $GITDIR
+    git fetch --tags
+    latest=(`git describe --tags --match "server-*" --abbrev=0 $(git rev-list --tags --max-count=1)`)
+    git checkout $latest
+}
+
+build(){
+    cd $WORKDIR
+    COPY .yarn/plugins ./.yarn/plugins
+    COPY .yarn/releases ./.yarn/releases
+    COPY package.json .
+    COPY .yarnrc.yml .
+    COPY yarn.lock .
+    COPY gulpfile.js .
+    COPY tsconfig.json .
+    COPY packages/turndown ./packages/turndown
+    COPY packages/turndown-plugin-gfm ./packages/turndown-plugin-gfm
+    COPY packages/fork-htmlparser2 ./packages/fork-htmlparser2
+    COPY packages/server/package*.json ./packages/server/
+    COPY packages/fork-sax ./packages/fork-sax
+    COPY packages/fork-uslug ./packages/fork-uslug
+    COPY packages/htmlpack ./packages/htmlpack
+    COPY packages/renderer ./packages/renderer
+    COPY packages/tools ./packages/tools
+    COPY packages/lib ./packages/lib
+    COPY packages/server ./packages/server
+
+    BUILD_SEQUENCIAL=1 yarn install --inline-builds \
+        && yarn cache clean \
+        && rm -rf .yarn/berry
+}
+
+install(){
+        cd $WORKDIR/
+        echo "installing packages to $JOPLIN_HOME/ ..."
+        rsync -a --delete ./packages/ $JOPLIN_HOME/packages/
+}
+
+run(){
+    APP_BASE_URL="${APP_BASE_URL:-https://joplin.me.org}"
+    APP_PORT="${APP_PORT:-22300}"
+    POSTGRES_PASSWORD="${POSTGRES_PASSWORD:-joplin}"
+    DB_CLIENT="${DB_CLIENT:-pg}"
+    POSTGRES_DATABASE="${POSTGRES_DATABASE:-joplin}"
+    POSTGRES_USER="${POSTGRES_USER:-joplin}"
+    POSTGRES_PORT="${POSTGRES_PORT:-5432}"
+    POSTGRES_HOST="${POSTGRES_HOST:-localhost}"
+    RUNNING_IN_DOCKER=0
+
+    export APP_BASE_URL APP_PORT DB_CLIENT POSTGRES_PASSWORD POSTGRES_DATABASE POSTGRES_USER POSTGRES_PORT POSTGRES_HOST RUNNING_IN_DOCKER
+
+    cd $JOPLIN_HOME
+    mkdir -p logs
+    npm --prefix packages/server start
+}
+
+case "$1" in
+    checkout-latest)
+        checkout_latest
+        ;;
+    build)
+        build
+        ;;
+    install)
+        install
+        ;;
+    makeall)
+        if systemctl is-active $SERVICE_NAME > /dev/null ;then
+            echo "$SERVICE_NAME still running as service - stop the service first"
+            exit 1
+        fi
+        checkout_latest
+        build
+        install
+        echo "DONE!"
+        ;;
+    run)
+        if systemctl is-active $SERVICE_NAME > /dev/null ;then
+            echo "$SERVICE_NAME still running as service - stop the service first"
+            exit 1
+        fi
+        run
+        ;;
+    *)
+        echo "Usage: $0 {checkout-latest|build|install|makeall|run}"
+        exit 1
+        ;;
+esac
